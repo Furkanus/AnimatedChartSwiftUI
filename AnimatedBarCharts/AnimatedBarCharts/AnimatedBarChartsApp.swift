a74095ce1a6f6670997c33bcfc5c1ca1da6e603d
Furkan Hanci <60580341+Furkanus@users.noreply.github.com>
2020-09-21 16:32:47 +0300

    Add files via upload

---
+++ AnimatedBarCharts/AnimatedBarCharts/AnimatedBarChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedBarChartsApp.swift
+//  AnimatedBarCharts
+//
+//  Created by Furkan Hanci on 9/21/20.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedBarChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
